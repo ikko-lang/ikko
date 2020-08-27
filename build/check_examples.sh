7a442e681db572fdaae9ad1b07eafb108057ba1a
Jeremy Mikkola <jeremy@jeremymikkola.com>
2020-08-26 22:49:47 -0400

    Check error examples

--- build/check_examples.sh
+++ build/check_examples.sh
@@ -10,6 +10,15 @@ for F in examples/*.ik; do
     fi
 done
 
+for F in examples/errors/*.ik; do
+    echo -n "."
+    # these *should* fail
+    if stack exec ikko -- "$F" 2> /dev/null; then
+        echo -e "\n$F should have failed but didn't"
+        FAILED=1
+    fi
+done
+
 if [ -n "$FAILED" ]; then
     echo -e "\nfailures"
     exit 1
