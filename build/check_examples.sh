3a4299277e93a94dac544abeddc8d2806cb62933
Jeremy Mikkola <jeremy@jeremymikkola.com>
2020-08-30 10:19:40 -0400

    Make check_examples run faser

--- build/check_examples.sh
+++ build/check_examples.sh
@@ -2,9 +2,11 @@
 
 cd $(dirname "$BASH_SOURCE[0]")/../
 
+BIN=".stack-work/install/x86_64-linux/157bf6244c9792b25fd59b58e6e4e87ef65813a154039f39d8a959445a467a47/8.6.5/bin/ikko"
+
 for F in examples/*.ik; do
     echo -n "."
-    if ! stack exec ikko -- "$F" > /dev/null; then
+    if ! $BIN "$F" > /dev/null; then
         echo -e "\n$F failed"
         FAILED=1
     fi
@@ -13,7 +15,7 @@ done
 for F in examples/errors/*.ik; do
     echo -n "."
     # these *should* fail
-    if stack exec ikko -- "$F" 2> /dev/null; then
+    if $BIN "$F" 2> /dev/null; then
         echo -e "\n$F should have failed but didn't"
         FAILED=1
     fi
